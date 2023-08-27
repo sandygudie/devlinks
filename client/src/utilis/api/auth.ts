eb6417ed1f1bea5a88ca7a25657fd81861d857e0
sandygudie <goodnewssandy@gmail.com>
2023-08-27 22:57:29 +0100

    fixes

--- client/src/utilis/api/auth.ts
+++ client/src/utilis/api/auth.ts
@@ -1,6 +1,10 @@
+import axios from 'axios'
 import makeApiCall from '.'
 
-
+export async function login() {
+  const response = await axios.get('https://devlinks-api.vercel.app/ap/v1/auth/google')
+  return response
+}
 export async function verification() {
   const response = await makeApiCall('/auth/google/success', 'get')
   return response
--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 let { MODE } = import.meta.env
+import {login} from "../utilis/api/auth"
 
 const googleLogin =
   MODE === 'development'
@@ -10,13 +11,13 @@ const googleLogin =
 <template>
   <div class="flex items-center justify-center h-screen gap-12 flex-col">
     <img src="@/assets/icons/logo-devlinks-small.svg" alt="devlink logo" width="100" height="100" />
-    <a
+    <div @click="login"
       class="shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] hover:shadow-[0_3px_10px_rgb(0,0,0,0.40)] font-extraBold flex items-center rounded-xl px-12 py-3"
-      :href="googleLogin"
+      
       target="_self"
     >
       <img src="@/assets/icons/google_icon.webp" alt="devlink logo" width="40" height="40" />
-      Sign in with Google</a
+      Sign in with Google</div
     >
   </div>
 </template>
