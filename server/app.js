38b1a76b1473ced6b0d2f8c7b33516ee008b5c30
sandygudie <goodnewssandy@gmail.com>
2023-08-30 13:31:33 +0100

    added fixes

--- server/app.js
+++ server/app.js
@@ -5,17 +5,8 @@ const apiRouter = require("./routes");
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL }));
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  next();
-});
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
