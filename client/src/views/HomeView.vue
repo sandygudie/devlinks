97027258c43c8ad9e685c0061153002e4fdaa184
sandygudie <goodnewssandy@gmail.com>
2023-09-30 13:11:58 +0100

    fixed loading

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -36,7 +36,7 @@ const profileLinks = ref<ProfileLinks>({
 
 let updatedLinks: { firstname: string; lastname: string; profilepic: any; devlinks: any }
 let errorList = ref<number[]>([])
-const isLoading = ref<boolean>(false)
+const isLoading = ref<boolean>(true)
 let userId: string | any
 const selected: number = 5
 let previewImage = ref('')
@@ -51,7 +51,6 @@ onMounted(async () => {
     return router.push('/login')
   }
   userId = existingUserId
-  isLoading.value = true
   try {
     const profileResponse = await getProfile(userId)
     if (profileResponse.success) {
