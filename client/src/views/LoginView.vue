3dadad1a20771f8439ed3e5eba47d2b3f148ab69
sandygudie <goodnewssandy@gmail.com>
2023-12-09 00:09:48 +0100

    fixed landing page

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -38,35 +38,46 @@ const login = () => {
   <span v-if="isLoading">
     <Spinner width="80px" height="80px" />
   </span>
-  <div v-else class="md:flex block fixed w-full overflow-hidden items-center justify-between">
+  <div v-else class="lg:flex block fixed w-full overflow-hidden items-center justify-between">
     <div
-      class="text-center h-screen flex items-center justify-center flex-col relative text-white text-center px-5 pt-4 md:p-10 bg-[#2839a3] md:w-[75%]">
-      <img className="w-10 block absolute top-5 left-5 md:hidden" src="@/assets/icons/logo-devlinks-small.svg"
-        alt="devlink logo" width="100" height="100" />
-      <div class="flex flex-col items-center md:justify-center md:h-full mt-4 md:mt-0">
+      class="text-center h-screen flex items-center md:justify-center flex-col relative text-white text-center px-5 pt-4 lg:p-10 bg-[#2839a3] lg:w-[75%]">
+      <div class="w-10 block absolute top-5 left-5 lg:hidden h-10">
+        <img className="w-full h-10" src="@/assets/icons/logo-devlinks-small.svg" alt="devlink logo" width="100"
+          height="100" loading="eager" />
+      </div>
+
+      <div class="flex flex-col mt-16 items-center lg:justify-center lg:h-full">
         <h1 class="text-4xl md:text-6xl font-bold">Stay Connected</h1>
         <p class="text-xl mt-2">Manage your social links in one place.</p>
-        <img className="w-[30rem] mx-auto mt-8" src="@/assets/images/social-media-collection.svg" alt="devlink logo"
-          width="100" height="100" />
+        <div class="h-60 md:w-[30rem] mt-6 mx-auto md:h-96">
+          <img className="w-full  h-60 md:h-96" src="../../public/assets/social-media-collection.svg" alt="devlink logo"
+            loading="eager" />
+        </div>
       </div>
-      <div class="flex mt-12 flex-col md:hidden items-center justify-center">
+      <div class="flex mt-12 flex-col lg:hidden items-center justify-center">
         <p class="text-xl mb-3">Log in to your account.</p>
 
         <button @click="login"
           class="bg-white text-black shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] hover:shadow-[0_3px_10px_rgb(0,0,0,0.40)] font-extraBold flex justify-between items-center rounded-xl px-6 py-2">
-          <img src="@/assets/icons/google_icon.webp" alt="devlink logo" width="40" height="40" />
+          <div class="w-10 h-10">
+            <img src="@/assets/icons/google_icon.webp" alt="devlink logo" loading="eager" width="40" height="40" />
+          </div>
           Sign in with Google
         </button>
       </div>
     </div>
-    <div class="w-1/2 mx-auto hidden md:block h-screen">
-      <img className="w-36 absolute top-10 right-10" src="@/assets/icons/logo-devlinks-large.svg" alt="devlink logo"
-        width="100" height="100" />
+    <div class="w-1/2 mx-auto hidden lg:block h-screen">
+      <div class="w-36 h-8 absolute top-8 right-10">
+        <img className="w-full h-8" src="../../public/assets/logo-devlinks-large.svg" alt="devlink logo"
+          loading="eager" />
+      </div>
       <div class="flex relative flex-col items-center h-full justify-center">
         <p class="text-xl mb-6">Log in to your account.</p>
         <button @click="login"
           class="bg-white text-sm shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] hover:shadow-[0_3px_10px_rgb(0,0,0,0.40)] font-extraBold flex justify-between gap-x-8 items-center rounded-xl pl-4 pr-10 py-2">
-          <img src="@/assets/icons/google_icon.webp" alt="devlink logo" width="40" height="40" />
+          <div class="w-10 h-10">
+            <img src="@/assets/icons/google_icon.webp" alt="devlink logo" width="40" loading="eager" height="40" />
+          </div>
           Sign in with Google
         </button>
       </div>
