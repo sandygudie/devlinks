1827aad8adca34860ebe7a4ff74b33c869e6970f
sandygudie <goodnewssandy@gmail.com>
2024-03-22 09:26:15 +0100

    fixes on client id

--- client/src/main.ts
+++ client/src/main.ts
@@ -8,7 +8,7 @@ import 'vue-select/dist/vue-select.css'
 import Vue3Toasity, { type ToastContainerOptions } from 'vue3-toastify'
 import 'vue3-toastify/dist/index.css'
 import vue3GoogleLogin from 'vue3-google-login'
-
+const clientId= import.meta.env.VITE_GOOGLE_CLIENT_ID
 const app = createApp(App)
 app.component('v-select', vSelect)
 
@@ -17,7 +17,7 @@ app.use(Vue3Toasity, {
   toastClassName: 'text-xs !min-h-[7px]'
 } as ToastContainerOptions)
 app.use(vue3GoogleLogin, {
-  clientId: '154494793944-q0j1j03tp2pjtlonk8b736n22if11ce7.apps.googleusercontent.com'
+  clientId: clientId
 })
 app.use(router)
 
